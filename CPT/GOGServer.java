4bb93011296a7170381466e6a171fb37a65ab047
Tony <tony.yao23@ycdsbk12.ca>
2022-05-27 11:54:08 -0400

    added CardLayout panel system for easier panel swapping
    also created some panels and moved most of the GUI stuff to GOGView to keep things consistent.

--- CPT/GOGServer.java
+++ CPT/GOGServer.java
@@ -6,28 +6,13 @@ import javax.swing.event.*;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 public class GOGServer implements ActionListener{
 	//Properties
-	public JFrame theFrame = new JFrame("Game of the Generals");
-	public GOGView thePanel = new GOGView();
-	public Timer theTimer = new Timer(1000/60, this);
-	public Timer theGameClockTimer=new Timer(1000,this);
+	public GOGView theGOGView = new GOGView();
 	public GOGModel theModel = new GOGModel();
 	public SuperSocketMaster ssm;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	//Methods
 	public void actionPerformed(ActionEvent evt){
-		//Ever 1/60th of a second, repaint
-		if(evt.getSource() == theTimer){
-			//we update the panel's array with the model array data
-			thePanel.strGOGArray=theModel.strArray;
-			thePanel.repaint();
-		}
-		
-		if(evt.getSource()==theGameClockTimer){
-			theModel.updateTime();
-			//This line has no purpose. I just wanted to save this so we know how to append the thing later
-			thePanel.drawNewTime(theModel.strPlayerTurn, theModel.intP1TimeLeft, theModel.intP2TimeLeft);
-		}
 		if(evt.getSource() == ssm){
 			//get text from player 2
 			String strText = ssm.readText();
@@ -36,22 +21,13 @@ public class GOGServer implements ActionListener{
 			//move the pices
 			theModel.checkPieceMovement();
 			//show this on the scroll plane
-			thePanel.theTextArea.append(strText + "\n");
+			theGOGView.theGamePanel.theTextArea.append(strText + "\n");
 		}
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////	
 	//Constructor
 	public GOGServer(){
-		thePanel.setPreferredSize(new Dimension(1280,720));
-		theFrame.setContentPane(thePanel);
-		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		theFrame.pack();
-		theFrame.setResizable(false);
-		theFrame.setVisible(true);
-		
-		theTimer.start();
-		theGameClockTimer.start();
 		ssm = new SuperSocketMaster(9001,this); //Server Constructor
 		ssm.connect();
 	}
--- CPT/GOGView.java
+++ CPT/GOGView.java
@@ -8,110 +8,55 @@ import javax.imageio.*;
 import java.io.*;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-public class GOGView extends JPanel{
+public class GOGView extends JPanel implements ActionListener {
 	//Properties
-	public String strGOGArray[][] = new String[8][9];
-	public BufferedImage imgChessboard = null;
-	
-	//Timer stuff
-	public JLabel theGameClockLabel=new JLabel("Time");
-	
-	//Text area stuff
-	public JTextArea theTextArea;
-	public JScrollPane theScroll;
+	public JFrame theFrame = new JFrame("Game of the Generals");
+	public CardLayout card = new CardLayout(); 
+	public JPanel theViewPanel = new JPanel(); 
+	public lobbyPanel theLobbyPanel = new lobbyPanel();
+	public gamePanel theGamePanel = new gamePanel(); 
+	public helpPanel theHelpPanel = new helpPanel();
+	public Timer theTimer = new Timer(1000/60, this);
+	public Timer theGameClockTimer=new Timer(1000,this);
+	public GOGModel theModel = new GOGModel();
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-	//Method
-	public void paintBackground(Graphics g){
-		g.setColor(Color.WHITE);
-		g.fillRect(0,0,1280,720);
-	}
-	
-	public void paintChessboard(Graphics g){
-		g.drawImage(imgChessboard,80,80,null);
-	}
-	
-	public void paintCharacters(Graphics g){
-		g.setColor(Color.BLACK);
-		g.drawString("1",110+70*0,50);
-		g.drawString("2",110+70*1,50);
-		g.drawString("3",110+70*2,50);
-		g.drawString("4",110+70*3,50);
-		g.drawString("5",110+70*4,50);
-		g.drawString("6",110+70*5,50);
-		g.drawString("7",110+70*6,50);
-		g.drawString("8",110+70*7,50);
-		g.drawString("9",110+70*8,50);
-	}
-	
-	public void paintPieces(Graphics g){
-		int intRow;
-		int intColumn;
-		g.setColor(Color.WHITE);
-		for(intRow=0;intRow<8;intRow++){
-			for(intColumn=0;intColumn<9;intColumn++){
-				if(strGOGArray[intRow][intColumn] == null){
-				}else if(strGOGArray[intRow][intColumn].equals("P1Private")){
-					g.drawString("p",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P1Spy")){
-					g.drawString("s",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P1Flag")){
-					g.drawString("f",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P1 Lieutenant 1st")){
-					g.drawString("L1",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P1 Lieutenant 2nd")){
-					g.drawString("L2",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P2Private")){
-					g.drawString("???",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P2Spy")){
-					g.drawString("???",110+70*intColumn,120+70*intRow);
-				}else if(strGOGArray[intRow][intColumn].equals("P2Flag")){
-					g.drawString("???",110+70*intColumn,120+70*intRow);
-				}
-			}
+	// Methods 
+	public void actionPerformed(ActionEvent evt){
+		//Ever 1/60th of a second, repaint
+		if(evt.getSource() == theTimer){
+			//we update the panel's array with the model array data
+			theGamePanel.strGOGArray=theModel.strArray;
+			theGamePanel.repaint();
 		}
-	}
-	
-	public void drawNewTime(String strPlayerTurn, int intP1TimeLeft, int intP2TimeLeft){
-		if(strPlayerTurn.equals("P1")){
-			int intMinutes = intP1TimeLeft/60;
-			int intSeconds = intP1TimeLeft%60;
-			theGameClockLabel.setText("Player 1 Time: "+intMinutes+":"+intSeconds);
-		}else if(strPlayerTurn.equals("P2")){
-			int intMinutes = intP2TimeLeft/60;
-			int intSeconds = intP2TimeLeft%60;
-			theGameClockLabel.setText("Player 2 Time: "+intMinutes+":"+intSeconds);
+		
+		if(evt.getSource()==theGameClockTimer){
+			theModel.updateTime();
+			//This line has no purpose. I just wanted to save this so we know how to append the thing later
+			theGamePanel.drawNewTime(theModel.strPlayerTurn, theModel.intP1TimeLeft, theModel.intP2TimeLeft);
 		}
-	}
+	}	
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		//Background
-		paintBackground(g);
-		paintChessboard(g);	
-		paintCharacters(g);	
-		paintPieces(g);
-	}
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////	
 	//Constructor
 	public GOGView(){
 		super();
-		try{
-			imgChessboard = ImageIO.read(new File("board.png"));
-		}catch(IOException e){
-			System.out.println("Error finding image");
-		}
-		this.setLayout(null);
+		theViewPanel.setLayout(card); 
+		theViewPanel.add(theLobbyPanel, "lobby");
+		theViewPanel.add(theGamePanel, "game");
+		theViewPanel.add(theHelpPanel, "help");
+		theViewPanel.setPreferredSize(new Dimension(1280,720));
 		
-		theGameClockLabel.setBounds(860,50,200,50);
-		this.add(theGameClockLabel);
+		theFrame.setContentPane(theViewPanel);
+		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.pack();
+		theFrame.setResizable(false);
+		theFrame.setVisible(true);
 		
-		theTextArea = new JTextArea();
-		theScroll = new JScrollPane(theTextArea);
-		theScroll.setBounds(720,150,550,500);
-		this.add(theScroll);
+		card.show(theViewPanel, "game");
+		
+		theTimer.start();
+		theGameClockTimer.start();
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
