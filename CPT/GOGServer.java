ffd45593dc230f1b949fcd12d84806dcf893ddb9
MehBirb <john.lu10212004@gmail.com>
2022-06-03 18:27:07 -0400

    Halfway done commenting the server file

--- CPT/GOGServer.java
+++ CPT/GOGServer.java
@@ -13,67 +13,144 @@ public class GOGServer extends GOGView implements ActionListener{
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	//Methods
 	public void actionPerformed(ActionEvent evt){
+		//Ever 1/60th of a second, the timer goes off and we can repaint and do any checks
 		if(evt.getSource()==theTimer){
+			//Repaint based on whichever panel we're on
 			theViewPanel.repaint();
+			
+			
+			//If it's time to send a message, then.....
 			if(theGamePanel.blnMessageSending==true){
+				//Put that message in our chatbox
 				theGamePanel.theTextArea.append(theGamePanel.theTextField.getText() + "\n");
+				
+				//Then send the message inside the spot we entered
 				ssm.sendText(theGamePanel.theTextField.getText());
+				
+				//We'll also blank the text field after they clicked enter
+				theGamePanel.theTextField.setText("");
+				
+				//After we sent the message, it's not time to send a message anymore
 				theGamePanel.blnMessageSending=false;
+				
+				
+			//If it's time to switch turns, then...
 			}else if(theGamePanel.blnSwitchTurn==true){
+				//Say it's time to switch turns in chatbox
 				theGamePanel.theTextArea.append(theModel.strPlayerTurn+" Turn now" + "\n");
+				
+				//Then send the updated player turn so it knows which turn it is
 				ssm.sendText(theModel.strPlayerTurn);
+				
+				//After this, we no longer need to switch a turn since it's already switched so falsify
 				theGamePanel.blnSwitchTurn=false;
+				
+				//After we switch turns, we should also update the array. If we switch turns, then
+				//We're also the ones sending data so blnSendArrayData becomes true.
 				theModel.blnSendArrayData=true;
+				
+				
+			//If it's time to update the other player's array, then...
 			}else if(theModel.blnSendArrayData==true){
+				//First, we will send this message that activates blnReceiveArrayData to true
+				//Which tells the receiver to receive array data.
 				ssm.sendText("Sending Data");
-				theModel.blnSendArrayData=false;
+				
+				//We create a loop that will send data in each column
 				int intRow;
 				int intClm;
 				int intLength;
 				for(intRow=0;intRow<8;intRow++){
 					for(intClm=0;intClm<9;intClm++){
+						//For them to know which index it is, we send the row and column to
+						//pinpoint the location in the array
 						ssm.sendText("Row:"+intRow);
 						ssm.sendText("Clm:"+intClm);
+						//then we send them information
 						ssm.sendText("Index:"+theModel.strArray[intRow][intClm]);
+						
+						//This isn't needed but it can be used to check
 						System.out.println("Index:"+theModel.strArray[intRow][intClm]);
 					}
 				}
+				
+				//Now that we are done updating the other player's array, we can stop sending data
+				theModel.blnSendArrayData=false;
+				
+				//We send this message which activates a command that turns the other player's
+				//blnReceiveArrayData variable off
 				ssm.sendText("Done Sending Data");
 			}
 		}
+		
+		//Ever second, we...
 		if(evt.getSource()==theGameClockTimer){
 			if(panelToReturn.equals("game")){
+				//Update the time based on who's turn it is
 				theModel.updateTime();
+				
+				//Then we show that time in the game panel
 				theGamePanel.drawNewTime(theModel.strPlayerTurn, theModel.intP1TimeLeft, theModel.intP2TimeLeft);
 			}
 		}
 		
-		// Network
+		//This is used for receiving network messages
 		if(evt.getSource() == ssm){
+			//We create a local variable that will help us store the sent message
 			String strText;
+			
+			//If it is time to receive array data information, then...
 			if(theModel.blnReceiveArrayData==true){
+				//We first store the send text inside strText
 				strText = ssm.readText();
-				int intLength;
+				
+				//If the sent message tell us that it is done sending info, then ...
 				if(strText.equals("Done Sending Data")){
+					//We now know that is is time to stop receiving information
 					theModel.blnReceiveArrayData=false;
+					
+				//If the first 4 characters day row:, then...
 				}else if(strText.substring(0,4).equals("Row:")){
+					//We can use intOGRow to store the row
 					theModel.intOGRow=Integer.parseInt(strText.substring(4,5));
+					
+				
+				//If the first 4 characters day clm:, then...
 				}else if(strText.substring(0,4).equals("Clm:")){
+					//We can use intOGClm to store the clm
 					theModel.intOGClm=Integer.parseInt(strText.substring(4,5));
+				
+				//If neither of those are the case, then it means this is a piece
 				}else{
-					intLength=strText.length();
+					//We may read the length
+					int intLength=strText.length();
+					
+					//We store the index in the strArray
 					theModel.strArray[theModel.intOGRow][theModel.intOGClm]=strText.substring(6,intLength);
 					theGamePanel.strGOGArray[theModel.intOGRow][theModel.intOGClm]=strText.substring(6,intLength);
+					
+					//This is not really important but will be kept to check in terminal
 					System.out.println(strText);
 				}
+			
+			//If it's not time to receive data, then...
 			}else{
-				//Recieve Info
+				//Read the text that was sent
 				strText = ssm.readText();
+				
+				//If it says P1 or P2, that means it is time to update the player turn
 				if(strText.equals("P1") || strText.equals("P2")){
+					//We update player turn here
 					theModel.strPlayerTurn=strText;
+					
+					//We will also tell the user in the chatbox that it is their turn now
 					theGamePanel.theTextArea.append(theModel.strPlayerTurn+" Turn now" + "\n");
+					
+				//If it says Sending Data, then it is time to receive data
 				}else if(strText.equals("Sending Data")){
+					//We turn receivedata on
 					theModel.blnReceiveArrayData=true;
+				//If none of this is the case, then we will put what is said in chatbox
 				}else{
 					theGamePanel.theTextArea.append(strText + "\n");
 				}
