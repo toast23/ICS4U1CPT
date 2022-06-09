378bb5e91e942de17a3a60b3579a08c2963a9958
MehBirb <john.lu10212004@gmail.com>
2022-06-09 12:15:13 -0400

    Update ranksPanel.java

--- CPT/ranksPanel.java
+++ CPT/ranksPanel.java
@@ -10,35 +10,67 @@ import java.io.*;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 public class ranksPanel extends JPanel{  
+	//Properties
+	Font theGOGFont = null;
 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	//Method
-
+	public Font loadFont(String strFileName, int intSize){    
+		Font theFont = null;
+		// Try to load the font
+		try{
+			theFont = Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream(strFileName)); 
+			return theFont.deriveFont(Font.PLAIN, intSize);
+		}catch(Exception e){
+		//System.out.println(e.toString());
+		}
+    
+		// Then try to load the font from the local filesystem
+		try{
+			theFont = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(strFileName)); 
+			return theFont.deriveFont(Font.PLAIN, intSize);
+		}catch(Exception e){
+			//System.out.println(e.toString());
+			System.out.println("Unable to load font file \""+strFileName+"\". Setting default font"); 
+		}
+    
+		// Then load the default font if all else fails
+		try{
+			theFont = Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("Hack-Regular.ttf")); 
+			return theFont.deriveFont(Font.PLAIN, 20);
+		}catch(Exception e){
+			//System.out.println(e.toString());
+			System.out.println("Unable to load default font file \"Hack-Regular.tff\".  Will default to Java's native font for your OS");
+		}
+		return theFont;
+	}
 	
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.drawString("Pieces              Ranks",100,100);
-		g.drawString("Flag                  1",100,130);
-		g.drawString("Spy                   2",100,160);
-		g.drawString("Private               3",100,190);
-		g.drawString("Seargent              4",100,220);
-		g.drawString("2nd_Lieutenant        5",100,250);
-		g.drawString("1st_Lieutenant        6",100,280);
-		g.drawString("Captain               7",100,310);
-		g.drawString("Major                 8",100,340);
-		g.drawString("Lieutenant_Colonel    9",100,370);
-		g.drawString("Colonel               10",100,400);
-		g.drawString("Brigadier_General     11",100,430);
-		g.drawString("Major_General         12",100,460);
-		g.drawString("Lieutenant General    13",100,490);
-		g.drawString("General               14",100,520);
-		g.drawString("General_of_the_Army   15",100,550);
+		g.setFont(theGOGFont);
+		g.setColor(Color.BLUE);
+		g.drawString("Pieces                            Ranks",100,40);
+		g.drawString("Flag                                  1",100,85);
+		g.drawString("Spy                                   2",100,130);
+		g.drawString("Private                             3",100,175);
+		g.drawString("Seargent                          4",100,220);
+		g.drawString("2nd_Lieutenant               5",100,265);
+		g.drawString("1st_Lieutenant                 6",100,305);
+		g.drawString("Captain                            7",100,355);
+		g.drawString("Major                               8",100,400);
+		g.drawString("Lieutenant_Colonel         9",100,455);
+		g.drawString("Colonel                            10",100,490);
+		g.drawString("Brigadier_General          11",100,535);
+		g.drawString("Major_General               12",100,580);
+		g.drawString("Lieutenant General          13",100,625);
+		g.drawString("General                            14",100,670);
+		g.drawString("General_of_the_Army   15",100,710);
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,6 +78,7 @@ public class ranksPanel extends JPanel{
 	public ranksPanel(){
 		super();
 		this.setLayout(null);
+		theGOGFont = this.loadFont("BEARPAW.TTF", 50);
 		this.setBackground(Color.WHITE);
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
