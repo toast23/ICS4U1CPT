58227d546d89db051f8764270f8479921c6f1d9a
MehBirb <john.lu10212004@gmail.com>
2022-06-10 10:33:36 -0400

    We now have images in the ranks panel

--- CPT/ranksPanel.java
+++ CPT/ranksPanel.java
@@ -11,10 +11,109 @@ import java.io.*;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 public class ranksPanel extends JPanel{  
 	//Properties
-	Font theGOGFont = this.loadFont("BEARPAW.TTF",50);;
+	Font theGOGFont = this.loadFont("BEARPAW.TTF",50);
+	public BufferedImage imgFlag = null;
+	public BufferedImage imgPrivate = null;
+	public BufferedImage imgSpies = null;
+	public BufferedImage imgFogOfWar = null;
+	public BufferedImage imgGeneral5 = null;
+	public BufferedImage imgGeneral4 = null;
+	public BufferedImage imgGeneral3 = null;
+	public BufferedImage imgGeneral2 = null;
+	public BufferedImage imgGeneral1 = null;
+	public BufferedImage imgColonel = null;
+	public BufferedImage imgLColonel = null;
+	public BufferedImage imgMajor = null;
+	public BufferedImage imgCaptain = null;
+	public BufferedImage imgLieutenant1 = null;
+	public BufferedImage imgLieutenant2 = null;
+	public BufferedImage imgSergeant = null;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	//Method
+	public void loadImages(String[] strImageArray){
+		try{
+			imgFlag = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[0]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgPrivate = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[1]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgSpies = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[2]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgSergeant = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[3]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgLieutenant2 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[4]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgLieutenant1 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[5]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgCaptain = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[6]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgMajor = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[7]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgLColonel = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[8]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgColonel = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[9]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgGeneral1 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[10]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgGeneral2 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[11]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgGeneral3 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[12]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgGeneral4 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[13]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgGeneral5 = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[14]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+		try{
+			imgFogOfWar = ImageIO.read(this.getClass().getResourceAsStream(strImageArray[15]));
+		}catch(IOException e){
+			System.out.println("Error finding image");
+		}
+	}
+	
 	/** The loadFont method allows us to use a specific font in our folder*/
 	public Font loadFont(String strFileName, int intSize){    
 		Font theFont = null;
@@ -47,36 +146,64 @@ public class ranksPanel extends JPanel{
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-	/** This method allows  */
+	/** This method will paint the first half of the pieces */
+	public void drawFirstHalf(Graphics g){
+		g.drawString("Pieces                            Ranks",50,50);
+		
+		g.drawString("Flag                                  1",50,130);
+		g.drawImage(imgFlag, 500, 100,null);
+		g.drawString("Spy                                   2",50,220);
+		g.drawImage(imgSpies, 500, 190,null);
+		g.drawString("Private                             3",50,305);
+		g.drawImage(imgPrivate, 500, 275,null);
+		g.drawString("Sergeant                          4",50,400);
+		g.drawImage(imgSergeant, 500, 370,null);
+		g.drawString("2nd_Lieutenant               5",50,490);
+		g.drawImage(imgLieutenant2, 500, 460,null);
+		g.drawString("1st_Lieutenant                 6",50,580);
+		g.drawImage(imgLieutenant1, 500, 550,null);
+		g.drawString("Captain                            7",50,670);
+		g.drawImage(imgCaptain, 500, 640,null);
+	}
+	
+	/** This method will paint the second half of the pieces */
+	public void drawSecondHalf(Graphics g){
+		g.drawString("Pieces                            Ranks",690,85);
+		g.drawString("Major                               8",690,130);
+		g.drawImage(imgMajor,1200,100,null);
+		g.drawString("Lieutenant_Colonel         9",690,205);
+		g.drawImage(imgColonel,1200,175,null);
+		g.drawString("Colonel                            10",690,280);
+		g.drawImage(imgColonel,1200,250,null);
+		g.drawString("Brigadier_General          11",690,355);
+		g.drawImage(imgGeneral1,1200,325,null);
+		g.drawString("Major_General               12",690,430);
+		g.drawImage(imgGeneral2,1200,400,null);
+		g.drawString("Lieutenant General          13",690,505);
+		g.drawImage(imgGeneral3,1200,475,null);
+		g.drawString("General                            14",690,580);
+		g.drawImage(imgGeneral4,1200,550,null);
+		g.drawString("General_of_the_Army   15",690,655);
+		g.drawImage(imgGeneral5,1200,625,null);
+	}
+	
+	/** This method allows us to draw the piece names, ranks, and images */
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setFont(theGOGFont);
 		g.setColor(Color.BLUE);
-		g.drawString("Pieces                            Ranks",100,40);
-		g.drawString("Flag                                  1",100,85);
-		g.drawString("Spy                                   2",100,130);
-		g.drawString("Private                             3",100,175);
-		g.drawString("Seargent                          4",100,220);
-		g.drawString("2nd_Lieutenant               5",100,265);
-		g.drawString("1st_Lieutenant                 6",100,305);
-		g.drawString("Captain                            7",100,355);
-		g.drawString("Major                               8",100,400);
-		g.drawString("Lieutenant_Colonel         9",100,455);
-		g.drawString("Colonel                            10",100,490);
-		g.drawString("Brigadier_General          11",100,535);
-		g.drawString("Major_General               12",100,580);
-		g.drawString("Lieutenant General          13",100,625);
-		g.drawString("General                            14",100,670);
-		g.drawString("General_of_the_Army   15",100,710);
+		this.drawFirstHalf(g);
+		this.drawSecondHalf(g);
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 	//Constructor
 	/** This method constructs the panel that will allow us to see the ranks*/
-	public ranksPanel(){
+	public ranksPanel(String[] strImageArray){
 		super();
 		this.setLayout(null);
 		this.setBackground(Color.WHITE);
+		this.loadImages(strImageArray);
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
