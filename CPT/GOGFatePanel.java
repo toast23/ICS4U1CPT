09417ebf5db880de4242f2578976b204db204d19
Nickgxxd <nicholasckwok@gmail.com>
2022-06-14 22:42:09 -0400

    More Comments

--- CPT/GOGFatePanel.java
+++ CPT/GOGFatePanel.java
@@ -17,8 +17,11 @@ public class GOGFatePanel extends JPanel{
 	//Methods
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		
+		/**When the string spells the word victory, then print the image for win*/
 		if(strFate.equals("victory")){
 			g.drawImage(imgWin,0,0,null);
+		/**On the other hand when the string prints defeat, print the image when you lose*/
 		}else if(strFate.equals("defeat")){
 			g.drawImage(imgLose,0,0,null);
 		}
@@ -26,6 +29,7 @@ public class GOGFatePanel extends JPanel{
 	
 	//Constructor
 	public GOGFatePanel(){
+		/**Exceptions for the images used for the win and lose screen at the end of the game*/
 		try{
 			imgWin = ImageIO.read(new File("Win background.png"));
 		}catch(IOException e){
