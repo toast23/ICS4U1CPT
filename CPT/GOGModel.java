66637b312835d97f54029625dba5155d0b0e515a
MehBirb <john.lu10212004@gmail.com>
2022-06-16 10:36:23 -0400

    Made more comments

--- CPT/GOGModel.java
+++ CPT/GOGModel.java
@@ -1,23 +1,43 @@
+/** The GOGModel class creates a class to store board data and player data */
 public class GOGModel{
 	//Properties
+	/** The strArray array is used to track the pieces on the board */
 	public String strArray[][] = GOGUtilities.makeEmptyBoardArray();
+	/** The strInfoArray is used to load info from name of pieces, ranks, and image names */
 	public String strInfoArray[][] = GOGUtilities.loadArray("ImageNames.csv");
+	/** the ingOGClm variable is used to keep track of the original column the piece that is picked up was in */
 	public int intOGClm;
+	/** the intOGRow variable is used to keep track of the original row the piece that is picked up was in */
 	public int intOGRow;
+	/** the ingNewClm variable is used to keep track of the new column the piece that is dropped in */
 	public int intNewClm;
+	/** the ingNewRow variable is used to keep track of the new row the piece that is dropped in */
 	public int intNewRow;
+	/** The intP1TimeLeft variable is used to keep track of how much time player 1 has */
 	public int intP1TimeLeft=7200;
+	/** The intP2TimeLeft variable is used to keep track of how much time player 2 has */
 	public int intP2TimeLeft=7200;
+	/** the strPlayerTurn variable tells us whose turn it is */
 	public String strPlayerTurn="P1";
+	/** The blnSwitchTurn variable tells us whether we should switch or not */
 	public boolean blnSwitchTurn = false;
+	/** The strVictor variable tells us whether someone won or not */
 	public String strVictor = " ";
+	/** The strActivePiece variable tells us what piece is actively being dragged */
 	public String strActivePiece;
+	/** The strNextPositionPiece variable tells us what piece is in the coordinate that the active piece was dragged too */
 	public String strNextPositionPiece;
+	/** The blnOnSameTeam variable tells us whether the piece we are going to is on the same team as us */
 	public boolean blnOnSameTeam=false;
+	/** The blnSendArrayData variable tells us whether we should send array data to the other player once their turn ends */
 	public boolean blnSendArrayData=false;
+	/** The blnGetName variable tells us when we should get the names of the other player */
 	public boolean blnGetName=false;
+	/** The strPlayer1Name variable stores the name of player 1 */
 	public String strPlayer1Name;
+	/** The strPlayer2Name variable stores the name of player 2 */
 	public String strPlayer2Name;
+	/** The strWarning variable stores the type of warning and will help warn the player if they do anything illegal */
 	String strWarning=" ";
 	//Methods
 	/** The updateTime method allows us to update the player time */
